fd8f3e0e82b2cdfd806859091391b3741f9db2c5
rockinrobin714 <robindykema@gmail.com>
2017-02-25 23:53:46 -0600

    Song list component

---
+++ spotify/src/components/SongList/SongList.js
@@ -0,0 +1,42 @@
+import React, { PropTypes } from 'react';
+
+import styles from './Songlist.css';
+
+const eachSong = (props) => {
+	const {album, name} = props;
+	const albumImage = album.images[0];
+	return (
+		<div className={styles.eachSong}>
+			<img role='presentation' 
+				src={albumImage.url}
+				className={styles.image}
+			/>
+			<span className={styles.songDescription}>Name{props.name}</span>
+		</div>
+	);
+};
+
+eachSong.propTypes = {
+	album: PropTypes.object,
+	name: PropTypes.string,
+};
+
+const SongList = (props) => (
+	<div className={styles.root}>
+		{props.listOfSongs.map((songData, index) => 
+			<div
+				key={songData.uri}
+				onClick={() => props.selectSong(index)}
+			>
+				{eachSong(songData)}
+			</div>
+			)}
+	</div>
+)
+
+SongList.propTypes = {
+	listOfSongs: PropTypes.array,
+	selectSong: PropTypes.func,
+}
+
+export default SongList;
--- spotify/src/components/app.js
+++ spotify/src/components/app.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import styles from './app.css';
 import SearchBar from './SearchBar/SearchBar.js';
 import SongItem from './SongItem/SongItem.js';
+import SongList from './SongList/SongList.js';
 
 import searchSpotify from '../utils/searchSpotify';
 
@@ -12,6 +13,7 @@ export default class App extends Component {
     this.state = {
       song: '',
       tracks: {},
+      songPosition: 0,
     };
   }
 
@@ -20,12 +22,13 @@ export default class App extends Component {
 			.then(( {tracks}) => this.setState({ tracks }))
 	}
   render() {
-    const { tracks } = this.state;
+    const { tracks, songPosition } = this.state;
 
     return (
       <div className={styles.root}>
       	<SearchBar fetchSongs={this.fetchSongs} updateText={(song) => this.setState({song})}/>
-        {tracks.items && <SongItem songData={tracks.items[0]} />}
+        {tracks.items && <SongItem songData={tracks.items[songPosition]} />}
+      	{tracks.items && <SongList listOfSongs={tracks.items} selectSong={(songPosition)=> this.setState({ songPosition })} />}
       </div>
     );
   }
