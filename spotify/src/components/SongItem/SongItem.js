a514df9c98c62a7975895f7f413ba8d3476c77e2
rockinrobin714 <robindykema@gmail.com>
2017-02-25 22:27:15 -0600

    Loads songs but no styling

--- spotify/src/components/SongItem/SongItem.js
+++ spotify/src/components/SongItem/SongItem.js
@@ -2,12 +2,28 @@ import React, { PropTypes } from 'react';
 import styles from './SongItem.css';
 
 const SongItem = (props) => {
-	console.log(props);
+	const { songData } = props;
+	const album = songData.album;
+	const albumName = album.name;
+	const albumImage = album.images[1];
 	return (
-		<div>
-			{this.props}
+		<div className={styles.root}>
+			<div className={styles.albun}>
+				<img role='presentation' src={albumImage.url} />
+				<span classNAme={styles.albumName}>{albumName}</span>
+			</div>
+			<div className={styles.songAndDescription}>
+				<audio controls duration src={songData.preview_url}/>
+				<span className={styles.songDescription}>Name: {songData.name}
+				</span>
+				<span className={styles.songDescription}>Duration: {songData.duration_ms / 1000}</span>
+			</div>
 		</div>
 	);
+};
+
+SongItem.PropTypes = {
+	songData: PropTypes.object,
 };
 
 export default SongItem;
