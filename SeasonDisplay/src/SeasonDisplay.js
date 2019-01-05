a9446d76892965672645b25302ddb0a935bb141b
rockinrobin714 <robindykema@gmail.com>
2019-01-05 11:36:27 -0600

    Create simple Season Display react app

---
+++ SeasonDisplay/src/SeasonDisplay.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './SeasonDisplay.css';
+
+const seasonConfig = {
+    summer: {
+        text: 'Let\'s hit the beach',
+        iconName: 'sun'
+    },
+    winter: {
+        text: 'Brr, it is chilly',
+        iconName: 'snowflake'
+    }
+}
+
+const getSeason = (lat, month) => {
+    if (month > 2 && month < 9) {
+        return lat > 0 ? 'summer' : 'winter';
+    }
+    return lat > 0 ? 'winter' : 'summer';
+}
+
+const SeasonDisplay = props => {
+    const season = getSeason(props.lat, new Date().getMonth());
+    const { text, iconName } = seasonConfig[season];
+    return (
+        <div className={`season-display ${season}`}>
+            <i className={`icon-left icon massive ${iconName}`} />
+            <h1>{text}</h1>
+            <i className={`icon-right icon massive ${iconName}`} />
+        </div>
+    );
+};
+
+export default SeasonDisplay;
