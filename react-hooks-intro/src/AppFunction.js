1d1fe0afcd233345b0d83338bd8c1ddab907828b
rockinrobin714 <robindykema@gmail.com>
2019-02-15 23:23:11 -0600

    Hooks

---
+++ react-hooks-intro/src/AppFunction.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+const App = () => {
+  const [count, setCount] = useState(0)
+  const incrementCount = () => {
+    setCount(count +1);
+  }
+  return (
+    <button onClick={incrementCount}>I was clicked {count} times</button>
+  )
+};
+
+export default App;
